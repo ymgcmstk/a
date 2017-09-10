f3efe7217d7b034a9bea98972162721e9b2c7bac
ymgcmstk <yamaguchi@mi.t.u-tokyo.ac.jp>
2017-09-10 18:32:07 +0900

    add files

---
+++ assets/memo_viewer.js
@@ -0,0 +1,52 @@
+$(function(){
+
+    var SIZES = ['small', false, 'large', 'huge'];
+    var cur_shown = 0;
+    var last_range = 0;
+    var server = $("#data").data("server");
+    var port = $("#data").data("port");
+    var paperid = $("#data").data("paperid");
+
+    // --------- basic functions ---------
+    function promote(cur_state, state_list) {
+        var i = state_list.indexOf(cur_state);
+        i = Math.min(i + 1, state_list.length - 1);
+        return state_list[i];
+    }
+    function demote(cur_state, state_list) {
+        var i = state_list.indexOf(cur_state);
+        i = Math.max(i - 1, 0);
+        return state_list[i];
+    }
+    // --------- basic functions ---------
+
+    var paper_id = location.href.split('/');
+    var paper_id = paper_id[paper_id.length-1];
+    console.log('Paper ID: ' + paper_id.toString());
+
+    var Delta = Quill.import('delta');
+    var quill = new Quill('#quill-container', {
+        modules: {
+            toolbar: false,
+            imageDrop: true, // https://github.com/kensnyder/quill-image-drop-module
+            keyboard: true,
+            imageResize: {
+                displaySize: true
+            }
+        },
+        scrollingContainer: '#quill-container',
+        placeholder: 'Take a note...',
+        theme: 'bubble'
+    });
+
+    // get data
+    var org_sum = decodeURI($('#data').text());
+    if (org_sum.length > 16) {
+        quill.setContents(JSON.parse(org_sum));
+    }
+
+    quill.enable(false)
+
+});
+
+// quill.insertEmbed(10, 'image', 'https://pbs.twimg.com/profile_images/378800000220029324/fe66faeca20115da8566e51d83447ead_400x400.jpeg');
---
+++ viewer.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from bottle import route, run, template, post, static_file, request, redirect
+from db_toolbox import *
+from settings import *
+from mytoolbox import get_html, full_listdir
+import commands
+import cv2
+import hashlib
+
+@route('/static/<filename1>/<filename2>')
+def get_static_file(filename1, filename2):
+    return static_file(filename2, root=os.path.join(DATA_DIR, filename1))
+
+@route('/assets/<filename>')
+def get_asset_file(filename):
+    return static_file(filename, root=FILE_DIR)
+
+@route('/')
+def index():
+    papers = get_papers_db()
+    return template('viewer.html',
+                    server=HOST_NAME,
+                    port=PORT_VIEWER,
+                    papers=papers)
+
+@route('/memo/<paper_id>')
+def memo(paper_id):
+    paper_info = get_paper_info_db(paper_id, PAPER_INFO)
+    if paper_info['summary'] is None:
+        paper_info['summary'] = ""
+    return template('memo_viewer.html',
+                    server=HOST_NAME,
+                    port=PORT_VIEWER,
+                    paper_info=paper_info)
+
+@route('/memo/crop/<paper_id>/<x>/<y>/<w>/<h>/<im_i>')
+def crop(paper_id, x, y, w, h, im_i):
+    x = int(x)
+    y = int(y)
+    w = int(w)
+    h = int(h)
+    image_i = int(im_i)
+    hash_str = '%d-%d-%d-%d-%d-%s' % (x, y, w, h, image_i, str(paper_id))
+    md5_hash = hashlib.md5(hash_str).hexdigest()
+    img_path = os.path.join(IMCACHE_DIR, md5_hash + '.jpg')
+    if os.path.exists(img_path):
+        touch(img_path)
+        return static_file(md5_hash + '.jpg', root=IMCACHE_DIR)
+        # return md5_hash + '.jpg'
+
+    org_path = os.path.join(DATA_DIR, 'data_' + str(paper_id), 'out-%d.jpg' % image_i)
+    assert os.path.exists(org_path)
+    img = cv2.imread(org_path)
+    crop_img = img[y:y+h, x:x+w]
+    cv2.imwrite(img_path, crop_img)
+    print md5_hash + '.jpg has been generated.'
+    return static_file(md5_hash + '.jpg', root=IMCACHE_DIR)
+
+if __name__ == '__main__':
+    run(host=HOST_NAME, port=PORT_VIEWER, debug=True, reloader=True)
