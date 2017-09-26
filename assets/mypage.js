5f2f385a045985a45153e387d1ad0c5fbcb12209
ymgcmstk <yamaguchi@mi.t.u-tokyo.ac.jp>
2017-09-26 23:26:44 +0900

    mod

--- assets/mypage.js
+++ assets/mypage.js
@@ -1,7 +1,8 @@
 $(function(){
     var targtd_length = $('.targtd').length;
-    $(document).on('keydown', '#notes', function(e) {
+    $(document).on('keydown', 'body', function(e) {
         var keycode = e.keyCode - 48;
+        if (document.activeElement.id.length > 1) return;
         if (keycode < targtd_length + 1 && keycode > 0) {
             e.preventDefault();
             window.location.href = $('#td' + keycode.toString()).attr('href');
